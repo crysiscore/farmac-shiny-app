9fa2b1a90bab6c1ef1de6d2e166ae66c1d606699
Agnaldo <agnaldosamuel3@mail.com>
2020-06-25 10:30:12 +0200

    deployment configuration

--- misc_scripts.R
+++ misc_scripts.R
@@ -10,5 +10,5 @@ source('db_con_functions.R')
 local.postgres.user ='postgres'                         # ******** modificar
 local.postgres.password='postgres'                      # ******** modificar
 local.postgres.db.name='pharm'                          # ******** modificar
-local.postgres.host='localhost'                        # ******** modificar
-local.postgres.port=5455                                # ******** modificar
+local.postgres.host='172.17.0.2'                        # ******** modificar
+local.postgres.port=5432                                # ******** modificar
