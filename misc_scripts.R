c23c50ec348d7b90e3108b97eb081bc6b58cbbbc
Agnaldo <agnaldosamuel3@mail.com>
2020-07-01 22:23:58 +0200

    bug fix - R creating weird column names when running app  on server

--- misc_scripts.R
+++ misc_scripts.R
@@ -8,7 +8,7 @@ source('db_con_functions.R')
 
 ## con farmac_sync
 local.postgres.user ='postgres'                         # ******** modificar
-local.postgres.password='postgres'                      # ******** modificar
-local.postgres.db.name='pharm'                          # ******** modificar
+local.postgres.password='iD@rt2020'                      # ******** modificar
+local.postgres.db.name='pharm'                           # ******** modificar
 local.postgres.host='172.17.0.2'                        # ******** modificar
 local.postgres.port=5432                                # ******** modificar
